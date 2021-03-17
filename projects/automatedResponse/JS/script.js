101fbc74838a29a73e92da3cf52e4373e137a8c2
christendeo <christen.deocampo90@qmail.cuny.edu>
2021-03-16 23:50:23 -0400

    Update v3.00

---
+++ projects/automatedResponse/JS/script.js
@@ -0,0 +1,14 @@
+const CTA = document.querySelector(".cta a");
+const ALERT = document.querySelector("#booking-alert");
+
+CTA.classList.remove("hide");
+ALERT.classList.add("hide");
+
+function reveal(e, current) {
+    e.preventDefault();
+    current.innerHTML == "Book Now!" ? CTA.innerHTML = "Oops!... I Did It Again" : CTA.innerHTML = "Book Now!";
+    ALERT.classList.toggle("hide");
+}
+
+CTA.addEventListener("click", function (e) {reveal(e, this); }, false);
+CTA.addEventListener("click", function () {console.log("The button was clicked!");}, false);
